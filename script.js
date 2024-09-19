f46a5c1fd075af95ebddb5f9428bd4b4cfdeb99f
Ramla Eman <ramla.eman.ra@gmail.com>
2024-09-19 12:32:05 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function convert() {
+    let tempValue = document.getElementById("temp-value").value;
+    let from = document.getElementById("from").value;
+    let to = document.getElementById("to").value;
+    let convertMsg = document.getElementById("msg-convert");
+
+    if (tempValue == "") {
+        convertMsg.innerText = `Enter a value to convert`;
+    }
+    else if (from === "fahrenheit" && to === "celsius") {
+        let calc = tempValue * (9 / 5) + 32;
+        convertMsg.innerText = `Converted Tempertaure :   ${calc.toFixed(2)} Celsius`;
+    }
+    else {
+        let calc2 = (tempValue - 32) * (5 / 9);
+        convertMsg.innerText = `Converted Tempertaure :   ${calc2.toFixed(2)} Fahrenheit`;
+    }
+}
+
+function reset() {
+    let fahrenheit = document.getElementById("fah");
+    let celsius = document.getElementById("cel");
+    document.getElementById("from").value = fahrenheit.value;
+    document.getElementById("to").value = celsius.value;
+    document.getElementById("temp-value").value = "";
+    document.getElementById("msg-convert").innerText = "";
+}
